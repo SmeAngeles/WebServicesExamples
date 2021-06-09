7e13d23b9449bb974f996c2dde37ea3f4a72967d
Esmeralda Angeles <esmeralda.angeles@bancoazteca.com>
2021-06-09 13:33:42 -0500

    Initial Commit

---
+++ WebServices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebServices
+//
+//  Created by Esmeralda Angeles Mendoza on 09/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
